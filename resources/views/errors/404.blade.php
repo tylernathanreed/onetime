42517083504a92784560ee82abae05a5dfe1f5d8
Tyler Reed <tylernathanreed@gmail.com>
2023-05-02 10:29:54 -0500

    ~ Don't translate

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Not Found'))
+@section('title', 'Not Found')
 @section('code', '404')
-@section('message', isset($exception) && !empty($exception->getMessage()) ? $exception->getMessage() : __('Not Found'))
+@section('message', isset($exception) && ! empty($exception->getMessage()) ? $exception->getMessage() : 'Not Found')
